7793ba80717e18f1dfe520d17c5acd9f3a6c8b1e
Shayan <shayan4shayan@gmail.com>
2020-12-05 08:51:47 +0330

    picker dialogs UI improved.

--- app/src/main/java/ir/shahinsoft/notifictionary/dialog/PickBoardDialog.kt
+++ app/src/main/java/ir/shahinsoft/notifictionary/dialog/PickBoardDialog.kt
@@ -2,6 +2,9 @@ package ir.shahinsoft.notifictionary.dialog
 
 import android.app.AlertDialog
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import ir.shahinsoft.notifictionary.R
 import ir.shahinsoft.notifictionary.adapters.CategoryAdapter
 import ir.shahinsoft.notifictionary.databinding.DialogPickBoardBinding
@@ -25,9 +28,50 @@ class PickBoardDialog(context:Context, val callback : (Board)->Unit) : AlertDial
     }
 
     private fun displayBoards(boards:List<Board>) {
-        binding.recycler.adapter = CategoryAdapter(ArrayList(boards)) {
+        binding.recycler.adapter = PickBoardAdapter(ArrayList(boards)) {
             callback(it)
             dismiss()
         }
     }
+}
+
+class PickBoardAdapter(val boards: List<Board>,val onBoardSelected: (Board)-> Unit) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>(){
+    override fun getItemViewType(position: Int): Int {
+        return ((position + 1) / 2) % 2
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryAdapter.ViewHolder {
+        return CategoryAdapter.ViewHolder(
+                if (viewType == 0) {
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_pick_category_primary, parent, false)
+                } else {
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_pick_category_secondary, parent, false)
+                }
+        ).apply {
+            itemView.setOnClickListener {
+                onBoardSelected(boards[adapterPosition])
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: CategoryAdapter.ViewHolder, position: Int) {
+        val board = boards[position]
+        holder.title.text = board.name
+        holder.leanredCount.text = "${board.learnedCount}"
+        holder.totalCount.text = "${board.totalCount}"
+        var progress = board.learnedCount.toFloat() / board.totalCount.toFloat()
+        if (board.totalCount ==0){
+            progress = 0f
+        }
+        if (progress < 0.33) {
+            holder.image.setImageResource(R.drawable.ic_boards_learning)
+        } else if (progress >= 0.33 && progress < 0.66) {
+            holder.image.setImageResource(R.drawable.ic_boards_learned_half)
+        } else {
+            holder.image.setImageResource(R.drawable.ic_board_learned)
+        }
+    }
+
+    override fun getItemCount() = boards.size
+
 }
